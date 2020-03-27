9c5ae171a087fbbb5ef39a13b1b0da0f874b2069
Tanguy Roberjot <tanguy.roberjot@eticasolucoes.com.br>
2020-03-27 15:01:15 -0300

    add pull to refresh

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -13,9 +13,30 @@ export default function Incidents() {
   const [total, setTotal] = useState(0);
   const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(false);
+  const [refreshing, setRefreshing] = useState(false);
 
   function navigateToDetails(incident){
     navigation.navigate('Details', { incident });
+  }
+
+  async function refreshIncidents(){
+    if(loading){
+      return;
+    }
+    setRefreshing(true);
+    setLoading(true);
+
+    try{
+      const response = await api.get('incidents');
+      setIncidents(response.data);
+      setPage(2);
+      setTotal(parseInt(response.headers['x-total-count']));
+    } catch(err) {
+      Alert.alert('Error', 'Error refreshing campaigns. Please try again.');
+    }
+
+    setLoading(false);
+    setRefreshing(false);
   }
 
   async function loadIncidents(){
@@ -52,35 +73,37 @@ export default function Incidents() {
       <View style={styles.header}>
         <Image source={logoImg}/>
         <Text style={styles.headerText}>
-          Total de <Text style={styles.headerTextBold}>{total} casos</Text>
+          Total of <Text style={styles.headerTextBold}>{total} campaigns</Text>
         </Text>
       </View>
 
-      <Text style={styles.title}>Bem-vindo!</Text>
-      <Text style={styles.description}>Escolha um dos casos abaixo e salve o dia.</Text>
+      <Text style={styles.title}>Welcome!</Text>
+      <Text style={styles.description}>Choose one of the following campaigns and save the day.</Text>
 
       <FlatList 
         data={incidents}
         keyExtractor={incident => incident.id.toString()}
         showsVerticalScrollIndicator={false}
+        refreshing={refreshing}
+        onRefresh={() => refreshIncidents()}
         onEndReached={loadIncidents}
         onEndReachedThreshold={0.2}
         style={styles.incidentList}
         renderItem={({ item: incident}) => (
           <View style={styles.incident}>
-            <Text style={styles.incidentProperty}>ONG:</Text>
+            <Text style={styles.incidentProperty}>NGO:</Text>
             <Text style={styles.incidentValue}>{incident.name}</Text>
 
 
-            <Text style={styles.incidentProperty}>CASO:</Text>
+            <Text style={styles.incidentProperty}>CAMPAIGN:</Text>
             <Text style={styles.incidentValue}>{incident.title}</Text>
 
 
-            <Text style={styles.incidentProperty}>VALOR:</Text>
+            <Text style={styles.incidentProperty}>VALUE:</Text>
             <Text style={styles.incidentValue}>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL'}).format(incident.value)}</Text>
 
             <TouchableOpacity style={styles.detailsButton} onPress={() => navigateToDetails(incident)}>
-              <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
+              <Text style={styles.detailsButtonText}>Show details</Text>
               <Feather name="arrow-right" size={16} color="#E02041"/>
             </TouchableOpacity>
           </View>
